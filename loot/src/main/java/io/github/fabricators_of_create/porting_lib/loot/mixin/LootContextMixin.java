8a77f641aadd5b73f7b6129fd8f2521ef0fcf884
TropheusJ <jverrellijr@icloud.com>
2023-08-04 22:10:06 +0200

    fix several problems with loot - fix #57 - fix #54

--- loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/mixin/LootContextMixin.java
+++ loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/mixin/LootContextMixin.java
@@ -2,10 +2,11 @@ package io.github.fabricators_of_create.porting_lib.loot.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 import io.github.fabricators_of_create.porting_lib.loot.LootTableIdCondition;
+import io.github.fabricators_of_create.porting_lib.loot.PortingLibLoot;
 import io.github.fabricators_of_create.porting_lib.loot.extensions.LootContextExtensions;
-import io.github.fabricators_of_create.porting_lib.util.PortingHooks;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
@@ -19,20 +20,22 @@ public abstract class LootContextMixin implements LootContextExtensions {
 	@Nullable
 	public abstract <T> T getParamOrNull(LootContextParam<T> lootContextParam);
 
+	@Unique
 	private ResourceLocation queriedLootTableId;
 
 	@Override
 	public void setQueriedLootTableId(ResourceLocation queriedLootTableId) {
-		if (this.queriedLootTableId == null && queriedLootTableId != null) this.queriedLootTableId = queriedLootTableId;
+		if (this.queriedLootTableId == null && queriedLootTableId != null)
+			this.queriedLootTableId = queriedLootTableId;
 	}
 
 	@Override
 	public ResourceLocation getQueriedLootTableId() {
-		return this.queriedLootTableId == null? LootTableIdCondition.UNKNOWN_LOOT_TABLE : this.queriedLootTableId;
+		return this.queriedLootTableId == null ? LootTableIdCondition.UNKNOWN_LOOT_TABLE : this.queriedLootTableId;
 	}
 
 	@Override
 	public int getLootingModifier() {
-		return PortingHooks.getLootingLevel(getParamOrNull(LootContextParams.THIS_ENTITY), getParamOrNull(LootContextParams.KILLER_ENTITY), getParamOrNull(LootContextParams.DAMAGE_SOURCE));
+		return PortingLibLoot.getLootingLevel(getParamOrNull(LootContextParams.THIS_ENTITY), getParamOrNull(LootContextParams.KILLER_ENTITY), getParamOrNull(LootContextParams.DAMAGE_SOURCE));
 	}
 }
--- loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/mixin/LootTableMixin.java
+++ loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/mixin/LootTableMixin.java
@@ -1,32 +1,54 @@
 package io.github.fabricators_of_create.porting_lib.loot.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.github.fabricators_of_create.porting_lib.loot.LootCollector;
 import io.github.fabricators_of_create.porting_lib.loot.extensions.LootTableExtensions;
-import io.github.fabricators_of_create.porting_lib.util.PortingHooks;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
 
+import java.util.Objects;
+import java.util.function.Consumer;
+
 @Mixin(LootTable.class)
 public class LootTableMixin implements LootTableExtensions {
+	@Unique
 	private ResourceLocation lootTableId;
 
 	@Override
 	public void setLootTableId(final ResourceLocation id) {
-		if (this.lootTableId != null) throw new IllegalStateException("Attempted to rename loot table from '" + this.lootTableId + "' to '" + id + "': this is not supported");
-		this.lootTableId = java.util.Objects.requireNonNull(id);
+		if (this.lootTableId != null)
+			throw new IllegalStateException("Attempted to rename loot table from '" + this.lootTableId + "' to '" + id + "': this is not supported");
+		this.lootTableId = Objects.requireNonNull(id);
 	}
 
 	@Override
-	public ResourceLocation getLootTableId() { return this.lootTableId; }
+	public ResourceLocation getLootTableId() {
+		return this.lootTableId;
+	}
 
-	@ModifyReturnValue(method = "getRandomItems(Lnet/minecraft/world/level/storage/loot/LootContext;)Lit/unimi/dsi/fastutil/objects/ObjectArrayList;", at = @At("RETURN"))
-	public ObjectArrayList<ItemStack> port_lib$modifyGlobalLootTable(ObjectArrayList<ItemStack> list, LootContext context) {
-		return PortingHooks.modifyLoot(getLootTableId(), list, context);
+	@ModifyVariable(
+			method = "getRandomItemsRaw(Lnet/minecraft/world/level/storage/loot/LootContext;Ljava/util/function/Consumer;)V",
+			at = @At("HEAD"),
+			argsOnly = true
+	)
+	private Consumer<ItemStack> wrapConsumer(Consumer<ItemStack> output) {
+		return new LootCollector(output);
+	}
+
+	@Inject(
+			method = "getRandomItemsRaw(Lnet/minecraft/world/level/storage/loot/LootContext;Ljava/util/function/Consumer;)V",
+			at = @At("RETURN")
+	)
+	private void finishCollectingLoot(LootContext context, Consumer<ItemStack> output, CallbackInfo ci) {
+		if (output instanceof LootCollector collector)
+			collector.finish(this.lootTableId, context);
 	}
 }
