7cb36a569f0d42e1d280e5fb9235c1137aba1a96
TropheusJ <jverrellijr@icloud.com>
2023-08-06 08:25:37 +0200

    fix loot table IDs not being set

--- loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/mixin/LootTableMixin.java
+++ loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/mixin/LootTableMixin.java
@@ -1,5 +1,6 @@
 package io.github.fabricators_of_create.porting_lib.loot.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,6 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.github.fabricators_of_create.porting_lib.loot.LootCollector;
+import io.github.fabricators_of_create.porting_lib.loot.extensions.LootTableBuilderExtensions;
 import io.github.fabricators_of_create.porting_lib.loot.extensions.LootTableExtensions;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -52,5 +54,22 @@ public class LootTableMixin implements LootTableExtensions {
 	private void finishCollectingLoot(LootContext context, Consumer<ItemStack> output, CallbackInfo ci) {
 		if (output instanceof LootCollector collector)
 			collector.finish(this.lootTableId, context);
+	}
+
+	@Mixin(LootTable.Builder.class)
+	public static class BuilderMixin implements LootTableBuilderExtensions {
+		@Unique
+		private ResourceLocation id;
+
+		@Override
+		public void port_lib$setId(ResourceLocation id) {
+			this.id = id;
+		}
+
+		@ModifyReturnValue(method = "build", at = @At("RETURN"))
+		private LootTable addId(LootTable table) {
+			table.setLootTableId(this.id);
+			return table;
+		}
 	}
 }
